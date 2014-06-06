4a44000a630e4bf29349087f14805a1f1b70a8ca
Jason Reilly <jdr0887@renci.org>
2014-06-06 16:02:01 -0400

    initial commit

---
+++ nec-variant-calling-messaging/src/main/java/edu/unc/mapseq/messaging/nec/variantcalling/NECVariantCallingMessageService.java
@@ -0,0 +1,75 @@
+package edu.unc.mapseq.messaging.nec.variantcalling;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NECVariantCallingMessageService {
+
+    private final Logger logger = LoggerFactory.getLogger(NECVariantCallingMessageService.class);
+
+    private Connection connection;
+
+    private Session session;
+
+    private ConnectionFactory connectionFactory;
+
+    private NECVariantCallingMessageListener messageListener;
+
+    private String destinationName;
+
+    public NECVariantCallingMessageService() {
+        super();
+    }
+
+    public void start() throws Exception {
+        logger.info("ENTERING start()");
+        this.connection = connectionFactory.createConnection();
+        this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = this.session.createQueue(this.destinationName);
+        MessageConsumer consumer = this.session.createConsumer(destination);
+        consumer.setMessageListener(getMessageListener());
+        this.connection.start();
+    }
+
+    public void stop() throws Exception {
+        logger.info("ENTERING stop()");
+        if (this.session != null) {
+            this.session.close();
+        }
+        if (this.connection != null) {
+            this.connection.stop();
+            this.connection.close();
+        }
+    }
+
+    public ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public NECVariantCallingMessageListener getMessageListener() {
+        return messageListener;
+    }
+
+    public void setMessageListener(NECVariantCallingMessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+}
